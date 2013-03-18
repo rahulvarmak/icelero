d06cde5b080d2f400a072bd794d6aee5005ac1cd
rahulvarmak <rahul@fissionlabs.in>
2013-03-18 09:18:45 +0000

    about_me cookbook

---
+++ cookbooks/about_me/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Fissionlabs"
+maintainer_email "rahul@fissionlabs.in"
+license          "All rights reserved"
+description      "Installs/Configures about_me"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "about_me::default","Prints hello world output"
---
+++ cookbooks/about_me/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: about_me
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hi, my name is Rahul"

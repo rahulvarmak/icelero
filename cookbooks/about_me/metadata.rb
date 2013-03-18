cf466f8bcc7f3208043e4442c62e6ba7d126a124
rahulvarmak <rahul@fissionlabs.in>
2013-03-18 09:31:13 +0000

    about_me cookbook

--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures about_me"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe "about_me::default","Prints hello world output"
+depends "rightscale"
+
+recipe "about_me::default","Prints my first name and several of my favorite things as output."

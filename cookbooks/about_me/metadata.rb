621d013ed03d03c165fd651561a97396de50dc1e
rahulvarmak <rahul@fissionlabs.in>
2013-03-18 08:06:45 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Fission"
+maintainer_email "rahul@fissionlabs.in"
+license          "All rights reserved"
+description      "Installs/Configures about_me"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"

e3bdcbea44e85e3eb206bcd8a2e2d4189a2555fb
rahulvarmak <rahul@fissionlabs.in>
2013-03-19 11:44:12 +0000

    cassandra

--- cookbooks/cassandra-chef-cookbook/metadata.rb
+++ cookbooks/cassandra-chef-cookbook/metadata.rb
@@ -6,4 +6,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
 
-recipe "cassandra-chef-cookbook::datastax","Installs Cassandra"
+recipe "cassandra-chef-cookbook::tarball","Installs Cassandra"

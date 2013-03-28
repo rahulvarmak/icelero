0eef52efa476de6c2b240660157fceac35a30365
rahulvarmak <rahul@fissionlabs.in>
2013-03-28 06:59:41 +0000

    cassandra install

--- cookbooks/devcassandra/recipes/default.rb
+++ cookbooks/devcassandra/recipes/default.rb
@@ -11,36 +11,36 @@ rightscale_marker :begin
 
 log "Installing Cassandra"
 
-execute "apt-get" do
- command "apt-get update"
- action :run
-end
-
-execute "apt-get" do
- command "apt-get install -y openjdk-6-jdk"
- action :run
-end
-
+#execute "apt-get" do
+# command "apt-get update"
+# action :run
+#end
 
 #execute "apt-get" do
-# command "apt-get install dsc12"
+# command "apt-get install -y openjdk-6-jdk"
 # action :run
 #end
 
-#execute "service" do
-# command "service cassandra stop"
-# action :run
-#end
 
-#execute "rm" do
-# command "rm -rf /var/lib/cassandra/data/system/*"
-# action :run
-#end
+execute "apt-get" do
+ command "apt-get install dsc12"
+ action :run
+end
 
-#execute "service" do
-# command "service cassandra start"
-# action :run
-#end
+execute "service" do
+ command "service cassandra stop"
+ action :run
+end
+
+execute "rm" do
+ command "rm -rf /var/lib/cassandra/data/system/*"
+ action :run
+end
+
+execute "service" do
+ command "service cassandra start"
+ action :run
+end
 
 
 

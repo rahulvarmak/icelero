92d217952902761a2b901a41fbed6418f99ee679
rahulvarmak <rahul@fissionlabs.in>
2013-03-28 06:19:33 +0000

    cassandra install

--- cookbooks/devcassandra/metadata.rb
+++ cookbooks/devcassandra/metadata.rb
@@ -5,4 +5,7 @@ description      "Installs/Configures devcassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
+
 recipe "devcassandra::default","installs cassandra"
